63d9b64008a8f7525bbe6fa15c8d36f9f881c850
manish <manish34dk@gmail.com>
2019-11-09 16:29:39 +0000

    Change to work with latest version of wrapper

--- Role Bot.py
+++ Role Bot.py
@@ -10,53 +10,64 @@ if os.path.isfile('keys.json'):
     with open('keys.json', 'r') as file_handle:
         KEY = json.load(file_handle)
 
-async def assign_member_role():
-    await CLIENT.wait_until_ready()
-    while not CLIENT.is_closed:
-        try:
-            games = {}
-            server_roles = []
-            ignore_games = ['Spotify']
-            members = list(CLIENT.get_all_members())
-            for member in members:
-                server = member.server
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.bg_task = self.loop.create_task(self.assign_member_role())
 
-                #Get roles from server
-                for role in server.roles:
-                    if role not in server_roles:
-                        server_roles.append(role.name)
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
 
-                #Create list of games that members are playing
-                if member.game == None or member.game.name in ignore_games:
-                    continue
-                if member.game.name not in games:
-                    games[member.game.name] = [member]
-                if member in games[member.game.name]:
-                    continue
-                else:
-                    games[member.game.name].append(member)
+    async def assign_member_role(self):
+        await self.wait_until_ready()
+        while not CLIENT.is_closed():
+            try:
+                games = {}
+                server_roles = []
+                ignore_games = ['Spotify', 'Custom Status']
+                members = list(CLIENT.get_all_members())
+                for member in members:
+                    server = CLIENT.guilds[0]
 
-            #Create server roles for games that 3+ members are playing
-            for game in games:
-                if game not in server_roles and len(games[game]) > 2:
-                    role = await CLIENT.create_role(server,name=game,mentionable=True)
-                    if role.name not in server_roles:
-                        server_roles.append(role.name)
+                    #Get roles from server
+                    for role in server.roles:
+                        if role not in server_roles:
+                            server_roles.append(role.name)
 
-            #Assign members to role
-            for game in games:
-                if game in server_roles:
-                    for member in games[game]:
-                        role_objects = server.roles
-                        for role_object in role_objects:
-                            if game == role_object.name:
-                                await CLIENT.add_roles(member,role_object)
-            await asyncio.sleep(10)
-        except:
-            await asyncio.sleep(120)
-            continue
+                    #Create list of games that members are playing
+                    if member.activity == None or member.activity.name in ignore_games:
+                        continue
+                    if member.activity.name not in games:
+                        games[member.activity.name] = [member]
+                    if member in games[member.activity.name]:
+                        continue
+                    else:
+                        games[member.activity.name].append(member)
+
+                #Create server roles for games that 3+ members are playing
+                for game in games:
+                    if game not in server_roles and len(games[game]) > 2:
+                        role = await server.create_role(name=game,mentionable=True)
+                        if role.name not in server_roles:
+                            server_roles.append(role.name)
+
+                #Assign members to role
+                for game in games:
+                    if game in server_roles:
+                        for member in games[game]:
+                            role_objects = server.roles
+                            for role_object in role_objects:
+                                if game == role_object.name:
+                                    await member.add_roles(role_object)
+                await asyncio.sleep(20)
+            except:
+                await asyncio.sleep(120)
+                continue
 
 
-CLIENT.loop.create_task(assign_member_role())
+CLIENT = MyClient()
 
-CLIENT.run(KEY['discord'])
+CLIENT.run('')
